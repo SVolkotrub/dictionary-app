aa52f5bf53f350d5eb9719434f23b7be137202c4
SVolkotrub <volkotrub.design@gmail.com>
2022-06-24 21:40:40 +0300

    added dictionary api integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,13 +1,21 @@
 import "./Dictionary.css";
 import React, { useState } from "react";
+import axios from "axios";
+
 
 export default function Dictionary() {
     const [keyword, setKeyword] = useState("");
 
     function search(event) {
         event.preventDefault();
-        alert(`Searching for ${keyword} definition`)
-
+        
+        let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+        axios(apiUrl).then(handleResponse).catch(function (error) {
+            return "Sorry, we cannot find this word in our database..😥";
+        })
+    }
+    function handleResponse(response) {
+        console.log(response.data[0]);
     }
     function handleKeywordChange(event) {
         setKeyword(event.target.value);
