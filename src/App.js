4ad453d35ae5409755ff9771575517e5c00cb845
SVolkotrub <volkotrub.design@gmail.com>
2022-06-24 21:22:03 +0300

    search engine

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import logo from "./images/Book_Lover.png";
 import './App.css';
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="container">
+        <header className="App-header">
         <img src={logo} alt="logo" className="App-logo img-fluid" />
-      </header>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">Coded by Svitlana Volkotrub</footer>
+      </div>
     </div>
   );
 }
