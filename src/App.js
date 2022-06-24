f4e7a88153070232aac727af60aba5acc9ea8b16
SVolkotrub <volkotrub.design@gmail.com>
2022-06-24 20:59:03 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./images/Book_Lover.png";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} alt="logo" className="App-logo img-fluid" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
