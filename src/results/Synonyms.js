b2c81b1791463130a910ec7bf98d1694e5743c03
SVolkotrub <volkotrub.design@gmail.com>
2022-06-27 18:52:02 +0300

    synonyms styling

--- src/results/Synonyms.js
+++ src/results/Synonyms.js
@@ -4,7 +4,7 @@ export default function Synonyms(props) {
     if (props.synonyms.length>0) {
         return (
             <div>
-               <strong className="header-synonyms">Synonyms: </strong> 
+               <strong className="header-synonyms">Synonyms: </strong> <br/>
                 {props.synonyms.map((synonym, index) => {
                 return (
                     <span className="synonyms" key={index}>
