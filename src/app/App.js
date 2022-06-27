9573add1d376e46663853241044cc20cfebdd5d7
SVolkotrub <volkotrub.design@gmail.com>
2022-06-28 01:14:24 +0300

    added links in footer

--- src/app/App.js
+++ src/app/App.js
@@ -13,7 +13,7 @@ function App() {
         <main className="container main">
           <Dictionary />
         </main>
-        <footer className="App-footer">Coded by Svitlana Volkotrub</footer>
+        <footer className="App-footer">Coded by <a href="https://portfolio-volkotrub.netlify.app/" target="_blank" rel="noreferrer noopener">Svitlana Volkotrub</a>, <a href="https://github.com/SVolkotrub/dictionary-app" rel="noreferrer noopener" target="_blank">open-source</a> project</footer>
       </div>
     </div>
   );
